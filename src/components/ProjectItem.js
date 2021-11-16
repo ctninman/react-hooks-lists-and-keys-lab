f7a694f07d0b1bc86941695a211cf33f5fe3f0f2
Chris Ninman <ctsnackpack@gmail.com>
2021-11-16 13:56:03 -0500

    commit tree c1d265fed226096b9065c7154bfd41edcc732f1f

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,10 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+        {technologies.map((technology) => {
+          return <span key={technology}>{technology}</span>
+        })}
+
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
@@ -13,3 +17,55 @@ function ProjectItem({ name, about, technologies }) {
 }
 
 export default ProjectItem;
+
+
+
+// import React from "react";
+
+// function ProjectItem({ name, about, technologies }) {
+//   const userTechnologies = technologies.map ((technology) => {
+//     return <span key={id} >{technology} </span> 
+//   })
+//     // console.log("technologies", technologies)
+
+
+//     return (
+//       <div className="project-item">
+//       <h3>{name}</h3>
+//       <p>{about}</p>
+//       <div className="technologies">
+//         {userTechnologies}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectItem;
+
+
+// import BookCard from "./BookCard"
+// import Genre from "./Genre";
+
+// function BookContainer(props){
+//     //TODO: Render BookCard dynamically for every book in props
+//     return(
+//         <>
+//             <Genre genreList={props.genreList}/>
+//             {props.bookList.map(book => <BookCard key={book.title} bookData={book}/>)}
+//             <BookCard />
+//             <BookCard />
+//         </>
+//     )
+// }
+
+// function Genre (props) {
+//     return (
+//       <>
+//         {props.genreList.map(genre => <div key={genre}> {genre}</div>)}
+//       </>
+//     )
+//   }
+  
+//   export default Genre
+
+// export default BookContainer
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,50 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  // const userProjects = projects.map(project => {
+  //   return <ProjectItem key={ project.id } />
+  // })
+  // console.log(userProjects)
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => {
+          return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>;
+        })}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
+
+
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+
+//   // const userProjects = projects.map((project) => {
+//   //   return <div key={project.id}>{project.name} <div/>
+//   // });
+
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list"> { userProjects } </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+
+
+
+
+
+{/* const userProjects = projects.map((project) => {
+  return <h1 key={user.id}>{user.firstName}</h1>;
+}); */}
